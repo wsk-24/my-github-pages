3d273deb59d2fc3514b0566d1f1fd075bd439e3f
Morphix-wsk <154492419+wsk-24@users.noreply.github.com>
2025-08-01 11:59:17 +0700

    Update
    - Change user interface

--- bk/my.js
+++
@@ -1,6 +0,0 @@
- // JavaScript to toggle languages
- function switchLang(lang) {
-    document.querySelectorAll('[data-lang]').forEach((element) => {
-        element.style.display = element.getAttribute('data-lang') === lang ? 'block' : 'none';
-    });
-}
